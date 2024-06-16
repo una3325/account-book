1b6b62c3a6cc941c71f4e82a5bb1d5fa8a1d529e
una3325 <a33256448@gmail.com>
2024-06-16 12:47:20 +0900

    CurrentAsset

--- js/components/current-asset.js
+++ js/components/current-asset.js
@@ -1,44 +1,57 @@
-import { store, updateStorage } from "../store";
-import { toHidden, toShow } from "../util";
+import { store, updateStorage } from '../store'
+import { toHidden, toShow } from '../util'
 
-const $currentAssetInput = document.querySelector(".current-asset-input");
-const $currentAssetValue = document.querySelector(".current-asset-value");
-const $currentAssetButton = document.querySelector(".current-asset-button");
-const $addItemButton = document.querySelector(".add-item-button");
+//Selector 앞에 $표시관습 : 앞에 변수가 DOM요소를 가르킨다.
+const $currentAssetInput = document.querySelector('.current-asset-input')
+const $currentAssetValue = document.querySelector('.current-asset-value')
+const $currentAssetButton = document.querySelector('.current-asset-button')
+const $addItemButton = document.querySelector('.add-item-button')
 
 export function initCurrentAsset() {
-  renderCurrentAsset();
-  addCurrentAssetEventListener();
+  renderCurrentAsset()
+  addCurrentAssetEventListener()
+  //ui를 심고, 렌더링 하는 부분이 필요하다.
 }
 
 function addCurrentAssetEventListener() {
-  $currentAssetValue.addEventListener("click", function (event) {
-    if (!store.isFirstEdit) return;
-    toHidden(event.target);
-    toShow($currentAssetInput);
-    toShow($currentAssetButton);
+  $currentAssetValue.addEventListener('click', function (event) {
+    if (!store.isFirstEdit) return
+    toHidden(event.target)
+    toShow($currentAssetInput)
+    toShow($currentAssetButton)
 
-    $currentAssetInput.focus();
-  });
+    $currentAssetInput.focus()
+  })
 
-  $currentAssetButton.addEventListener("click", function (event) {
-    toHidden(event.target);
-    toHidden($currentAssetInput);
-    toShow($currentAssetValue);
-    toShow($addItemButton);
+  $currentAssetButton.addEventListener('click', function (event) {
+    toHidden(event.target)
+    toHidden($currentAssetInput)
+    toShow($currentAssetValue)
+    toShow($addItemButton)
 
-    store.currentFunds = Number($currentAssetInput.value);
-    renderCurrentAsset();
+    store.currentFunds = Number($currentAssetInput.value)
+    renderCurrentAsset()
 
-    store.isFirstEdit = false;
+    store.isFirstEdit = false
 
-    updateStorage();
-  });
+    updateStorage()
+  })
 }
 
 export function renderCurrentAsset() {
   // TODO: 숫자에 콤마 작성
   // TODO: currentFunds가 없는 경우
-  $currentAssetValue.textContent = store.currentFunds ?? "-";
-  $currentAssetInput.value = store.currentFunds;
+  // 1. if문
+  // if (store.currentFunds) {
+  //   $currentAssetValue.textContent = store.currentFunds.toLocaleString() ?? '-'
+  // }
+
+  // 2. 삼항 연산자
+  // $currentAssetValue.textContent = store.currentFunds
+  //   ? store.currentFunds.toLocaleString()
+  //   : '-'
+
+  //3. Optional Chaining
+  $currentAssetValue.textContent = store.currentFunds?.toLocaleString() ?? '-'
+  $currentAssetInput.value = store.currentFunds
 }
