874c088c39686b7e64192eafa6eeb847e22949ba
una3325 <a33256448@gmail.com>
2024-06-16 12:48:37 +0900

    validation - 소비 내역 작성

--- js/util.js
+++ js/util.js
@@ -1,17 +1,26 @@
 export function toShow(node) {
-  node.className = node.className.replace("v-none", "v-show");
+  node.className = node.className.replace('v-none', 'v-show')
 }
 
 export function toHidden(node) {
-  node.className = node.className.replace("v-show", "v-none");
+  node.className = node.className.replace('v-show', 'v-none')
 }
 
 export function validatePrice(currentFunds, currentAmount) {
+  if (currentFunds >= currentAmount) {
+  }
   // TODO: 금액이 현재 자산보다 이하인지
-  return true;
+  return currentFunds >= currentAmount
 }
 
 export function validateRequired({ category, description, price }) {
   // TODO: 값이 존재하는지
-  return true;
+  //"", null, undefined
+  // 0
+  //1. Boolean 사용
+  return (
+    Boolean(category) && Boolean(description) && Boolean(price) && price > 0
+  )
+  //2. !! 사용
+  // return !!category
 }
