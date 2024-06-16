3bfae7d475b946b9b2c79d541f0424a5a0834b10
una3325 <a33256448@gmail.com>
2024-06-16 12:54:26 +0900

    add docs, refactoring

--- js/index.js
+++ js/index.js
@@ -6,9 +6,9 @@ import { initAddItem } from "./components/add-item";
 init();
 
 function init() {
-  initStore();
+  initStore(); //스토어 객체 초기화 함수
 
-  initCurrentAsset();
-  initAddItem();
-  initHistoryList();
+  initCurrentAsset(); //현재 자산 입력 함수 초기화 
+  initAddItem(); //새로운 아이템을 추가기능 초기화 
+  initHistoryList(); //히스토리 기능 초기화, 과거 자산 기록 불러오기
 }
